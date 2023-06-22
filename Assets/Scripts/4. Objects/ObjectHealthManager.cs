fb845519462fc08c46f4557debeb20415d489481
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2023-06-22 15:57:03 +1000

    update object spawner

--- Assets/Scripts/4. Objects/ObjectHealthManager.cs
+++ Assets/Scripts/4. Objects/ObjectHealthManager.cs
@@ -7,17 +7,30 @@ public class ObjectHealthManager : MonoBehaviour
     [SerializeField] private int maxHealth = 6;
     public int health = 0;
 
+    // Flashing effect variables
+    [SerializeField] private float flashDuration = 0.1f;
+    [SerializeField] private Color flashColor = Color.red;
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+
+    // Exploding effect variables
+    public GameObject explosionPrefab;
+
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
         health = maxHealth;
     }
 
     public void DamageObject(int damage)
     {
         health -= damage;
+        StartCoroutine(FlashEffect());
 
-        if (health <= 0)
+        if(health <= 0)
         {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             DestroyObject();
         }
     }
@@ -25,5 +38,13 @@ public class ObjectHealthManager : MonoBehaviour
     private void DestroyObject()
     {
         Destroy(gameObject);
+    }
+    
+    private IEnumerator FlashEffect()
+    {
+        Debug.Log("Flash");
+        spriteRenderer.color = flashColor;
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = originalColor;
     }
 }
