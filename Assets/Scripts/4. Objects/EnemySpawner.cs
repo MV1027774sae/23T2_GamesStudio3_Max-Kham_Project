304259135be3ab6f134e558a31b012520c3c18a4
Max Valentine <107592271+1027774sae@users.noreply.github.com>
2023-06-27 13:06:52 +1000

    Updated Enemy Spawner, now limited

--- Assets/Scripts/4. Objects/EnemySpawner.cs
+++ Assets/Scripts/4. Objects/EnemySpawner.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject spawnableEnemy;
+    [SerializeField] private GameObject[] spawnableEnemy;
 
-    [SerializeField] private float spawnRate;
+    [SerializeField] private float spawnRate = 3f;
     [SerializeField] private float spawnRange = 4;
+    private bool canSpawn = true;
+    [SerializeField] private int totalEnemiesSpawned = 0;
+    [SerializeField] private int enemySpawnLimit = 10;
 
     void Start()
     {
-        StartCoroutine(SpawnEnemy(spawnRate, spawnableEnemy));
+        StartCoroutine(SpawnEnemy());
     }
 
-    private IEnumerator SpawnEnemy(float interval, GameObject enemy)
+    private IEnumerator SpawnEnemy()
     {
-        yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(gameObject.transform.position.x + Random.Range(-spawnRange, spawnRange), gameObject.transform.position.y + Random.Range(-spawnRange, spawnRange), 0), Quaternion.identity);
-        StartCoroutine(SpawnEnemy(interval, enemy));
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        while (canSpawn && totalEnemiesSpawned <= enemySpawnLimit)
+        {
+            yield return wait;
+            int rand = Random.Range(0, spawnableEnemy.Length);
+            GameObject enemyToSpawn = spawnableEnemy[rand];
+
+            Instantiate(enemyToSpawn, new Vector3(gameObject.transform.position.x + Random.Range(-spawnRange, spawnRange), gameObject.transform.position.y + Random.Range(-spawnRange, spawnRange), 0), Quaternion.identity);
+            totalEnemiesSpawned++;
+        }
     }
 }
