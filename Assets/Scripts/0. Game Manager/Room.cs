cd0bae28c15ef955d494214aee6205c344395e94
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2023-06-29 19:27:56 +1000

    update playtest scene

--- Assets/Scripts/0. Game Manager/Room.cs
+++ Assets/Scripts/0. Game Manager/Room.cs
@@ -6,16 +6,20 @@ public class Room : MonoBehaviour
 {
     public GameObject virtualCam;
     public GameObject enemySpawner;
+    private Renderer spawnerRenderer;
 
-    private bool playerInRoom = false;
+    private void Start()
+    {
+        // Get the renderer component of the enemy spawner
+        spawnerRenderer = enemySpawner.GetComponent<Renderer>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             virtualCam.SetActive(true);
-            playerInRoom = true;
-            enemySpawner.SetActive(playerInRoom);
+            spawnerRenderer.enabled = true; // Enable the renderer to make the spawner visible
         }
     }
 
@@ -24,9 +28,11 @@ public class Room : MonoBehaviour
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             virtualCam.SetActive(false);
-            playerInRoom = false;
-            enemySpawner.SetActive(playerInRoom);
+            spawnerRenderer.enabled = false; // Disable the renderer to hide the spawner
         }
     }
 }
+
+
+
 
