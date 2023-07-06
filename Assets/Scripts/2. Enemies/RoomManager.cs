68d404ca7d3cbd45c071db25f225f1ac36dd4851
Max Valentine <107592271+MV1027774sae@users.noreply.github.com>
2023-07-06 13:43:46 +1000

    Room manager

---
+++ Assets/Scripts/2. Enemies/RoomManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _enemiesInRoom = new List<GameObject>();
+
+    private BoxCollider2D BoxCollider;
+
+    void Start()
+    {
+        BoxCollider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (!_enemiesInRoom.Contains(GameObject.FindGameObjectWithTag("Enemy")))
+        {
+            Debug.Log("No enemies remaining");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            _enemiesInRoom.Add(collision.gameObject);
+        }
+    }
+}
