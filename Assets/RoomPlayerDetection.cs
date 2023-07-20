7dc033b2d935a3746d4748f263988aea311d9428
Max Valentine <107592271+1027774sae@users.noreply.github.com>
2023-07-20 23:57:11 +1000

    Playtest Prep

---
+++ Assets/RoomPlayerDetection.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class RoomPlayerDetection : MonoBehaviour
+{
+    private float overlapBoxSizeX;
+    private float overlapBoxSizeY;
+    [SerializeField] private LayerMask enemyLayer;
+    void Start()
+    {
+        overlapBoxSizeX = transform.localScale.x;
+        overlapBoxSizeY = transform.localScale.y;
+        DisableEnemies();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered");
+        if (collision.tag == "Player")
+        {
+            EnableEnemies();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exited");
+        if (collision.tag == "Player")
+        {
+            DisableEnemies();
+        }
+    }
+
+    private void EnableEnemies()
+    {
+        Collider2D[] enemiesFound = Physics2D.OverlapBoxAll(transform.position, new Vector2(overlapBoxSizeX, overlapBoxSizeY), 0, enemyLayer);
+
+        foreach (Collider2D enemy in enemiesFound)
+        {
+            if (enemy.gameObject.GetComponent<AIPath>() != null)
+            {
+                enemy.gameObject.GetComponent<AIPath>().enabled = true;
+            }
+            if (enemy.gameObject.GetComponent<EnemySpawner>() != null)
+            {
+                enemy.gameObject.GetComponent<EnemySpawner>().enabled = true;
+                enemy.gameObject.GetComponent<EnemySpawner>().canSpawn = true;
+                StartCoroutine(enemy.gameObject.GetComponent<EnemySpawner>().SpawnEnemy());
+            }       
+        }
+    }
+
+    private void DisableEnemies()
+    {
+        Collider2D[] enemiesFound = Physics2D.OverlapBoxAll(transform.position, new Vector2(overlapBoxSizeX, overlapBoxSizeY), 0, enemyLayer);
+
+        foreach(Collider2D enemy in enemiesFound)
+        {
+            if (enemy.gameObject.GetComponent<AIPath>() != null) 
+            { 
+                enemy.gameObject.GetComponent<AIPath>().enabled = false;
+            } 
+            if (enemy.gameObject.GetComponent<EnemySpawner>() != null)
+            {
+                enemy.gameObject.GetComponent<EnemySpawner>().canSpawn = false;
+                enemy.gameObject.GetComponent<EnemySpawner>().enabled = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/4. Objects/EnemySpawner.cs
+++ Assets/Scripts/4. Objects/EnemySpawner.cs
@@ -6,7 +6,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject[] spawnableEnemy;
     [SerializeField] private float spawnRate = 3f;
     [SerializeField] private float spawnRange = 4f;
-    private bool canSpawn = false;
+    public bool canSpawn = false;
     [SerializeField] private int totalEnemiesSpawned = 0;
     [SerializeField] private int enemySpawnLimit = 10;
 
@@ -22,33 +22,33 @@ public class EnemySpawner : MonoBehaviour
         StartCoroutine(SpawnEnemy());
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player entered the spawn range.");
-            canSpawn = true;
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Debug.Log("Player entered the spawn range.");
+    //        canSpawn = true;
+    //    }
+    //}
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player exited the spawn range.");
-            canSpawn = false;
-        }
-    }
+    //private void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Debug.Log("Player exited the spawn range.");
+    //        canSpawn = false;
+    //    }
+    //}
 
     public IEnumerator SpawnEnemy()
     {
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
         int enemiesSpawned = 0;
 
-        while (enemiesSpawned < enemySpawnLimit)
+        while (enemiesSpawned < enemySpawnLimit && canSpawn)
         {
             // Wait until spawning is allowed and the player is inside the spawn range
-            yield return new WaitUntil(() => canSpawn);
+            //yield return new WaitUntil(() => canSpawn);
 
             yield return wait;
 
