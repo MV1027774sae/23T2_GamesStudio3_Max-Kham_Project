3aaeec4c30f88fc96ae711a2bf93664c5a7efdaf
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2023-07-26 18:47:29 +1000

    Level 1 update basic

--- Assets/Scripts/0. Game Manager/TilemapController.cs
+++ Assets/Scripts/0. Game Manager/TilemapController.cs
@@ -3,56 +3,83 @@ using UnityEngine.Tilemaps;
 
 public class TilemapController : MonoBehaviour
 {
-    public Tile closedDoorTile;
-    public Tile openDoorTile;
-    public Tilemap tilemap;
-    public Transform teleportDestination;
-    public BoxCollider2D doorCollider;
+    //public Tile closedDoorTile;
+    //public Tile openDoorTile;
+    //public Tilemap tilemap;
+    //public Transform teleportDestination;
+    //public BoxCollider2D doorCollider;
 
-    public GameObject[] enemies;
+    //private bool doorOpen = false;
+    //private bool enemiesPresent = false; // Added variable to track the presence of enemies
 
-    private bool doorOpen = false;
+    //private void Start()
+    //{
+    //    tilemap = GetComponent<Tilemap>();
+    //    doorCollider = GetComponent<BoxCollider2D>();
+    //}
 
-    private void Start()
-    {
-        tilemap = GetComponent<Tilemap>();
-        doorCollider = GetComponent<BoxCollider2D>();
-    }
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        TeleportPlayerToDestination(other.gameObject);
+    //    }
+    //}
 
-    private void Update()
-    {
-        // Check if there are any enemies in the room
-        enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        bool hasEnemies = enemies.Length > 0;
+    //private void Update()
+    //{
+    //    // Check if there are any enemies in the room
+    //    GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    //    bool hasEnemies = enemies.Length > 0;
 
-        if (hasEnemies && doorOpen)
-        {
-            ChangeTile(closedDoorTile, false);
-            doorOpen = false;
-            Debug.Log("close the door");
-        }
-        else if (!hasEnemies && !doorOpen)
-        {
-            ChangeTile(openDoorTile, true);
-            doorOpen = true;
-            Debug.Log("open the door");
-        }
-    }
+    //    if (hasEnemies && doorOpen)
+    //    {
+    //        // Change tile to closedDoorTile when enemies are present
+    //        tilemap.SwapTile(openDoorTile, closedDoorTile);
+    //        doorOpen = false;
+    //        Debug.Log("Close the door");
+    //    }
+    //    else if (!hasEnemies && !doorOpen)
+    //    {
+    //        // Change tile to openDoorTile when no enemies
+    //        tilemap.SwapTile(closedDoorTile, openDoorTile);
+    //        doorOpen = true;
+    //        Debug.Log("Open the door");
+    //    }
+    //}
 
-    private void ChangeTile(Tile tile, bool isTrigger)
-    {
-        tilemap.SwapTile(closedDoorTile, openDoorTile);
-        doorCollider.isTrigger = isTrigger;
-    }
+    //private void TeleportPlayerToDestination(GameObject player)
+    //{
+    //    Transform teleportDestination = transform.Find("DoorDestination");
+    //    if (teleportDestination != null)
+    //    {
+    //        player.transform.position = teleportDestination.position;
+    //    }
+    //}
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.transform.position = teleportDestination.position;
-        }
-    }
+    //// Method to set the presence of enemies in the room
+    //public void SetEnemiesPresent(bool present)
+    //{
+    //    if (present && doorOpen)
+    //    {
+    //        // Change tile to closedDoorTile when enemies are present
+    //        tilemap.SwapTile(openDoorTile, closedDoorTile);
+    //        doorOpen = false;
+    //        Debug.Log("Close the door");
+    //    }
+    //    else if (!present && !doorOpen)
+    //    {
+    //        // Change tile to openDoorTile when no enemies
+    //        tilemap.SwapTile(closedDoorTile, openDoorTile);
+    //        doorOpen = true;
+    //        Debug.Log("Open the door");
+    //    }
+    //}
 }
+
+
+
+
 
 
 
