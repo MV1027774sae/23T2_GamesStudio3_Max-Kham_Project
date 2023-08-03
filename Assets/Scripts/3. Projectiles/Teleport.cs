7ff78ad651dffd9f6f9150a14ffa4212d6ae0f4e
KhamHuyLu <85721895+KhamHuyLu@users.noreply.github.com>
2023-08-03 15:43:49 +1000

    update level 2

--- Assets/Scripts/3. Projectiles/Teleport.cs
+++ Assets/Scripts/3. Projectiles/Teleport.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    public Transform teleportDestination; // Reference to the destination transform where the object will be teleported
-
-    // Called when the object enters the teleporter trigger
     private void OnTriggerEnter2D(Collider2D other)
     {
-        TeleportObject(other.transform);
+        MoveToNextScene();
     }
 
-    // Teleport the object to the destination position
-    private void TeleportObject(Transform objectToTeleport)
+    // Move to the next scene
+    private void MoveToNextScene()
     {
-        if (teleportDestination != null && objectToTeleport != null)
+        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings)
         {
-            objectToTeleport.position = teleportDestination.position;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No next scene available. You have reached the end of the build settings scene list.");
         }
     }
 }
