40141f146606a13ace57c43350189aed8cba3d61
Max Valentine <107592271+MV1027774sae@users.noreply.github.com>
2023-08-11 13:31:52 +1000

    Dash bug fixes + Collider rebalancing

--- Assets/Scripts/3. Projectiles/DashLifetime.cs
+++ Assets/Scripts/3. Projectiles/DashLifetime.cs
@@ -2,24 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrimaryFireExplosion : MonoBehaviour
+public class DashLifetime : MonoBehaviour
 {
     [SerializeField] private float lifeTime = 0.3f;
-    [SerializeField] private AudioClip explosionSFX;
-    private AudioSource audioSource;
-   
+
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         StartCoroutine(DeathDelay());
     }
 
     IEnumerator DeathDelay()
     {
-        if (explosionSFX != null)
-        {
-            audioSource.PlayOneShot(explosionSFX);
-        }
         yield return new WaitForSeconds(lifeTime);
         Destroy(gameObject);
     }
